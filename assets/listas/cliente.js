4ad05385ad83cff3f9e2fd0bba8afb02bb701371
Edmundo Triguero <edmundotriguero@gmail.com>
2019-06-27 16:40:48 -0400

    primer commit, se esta enviando tambien imagenes. Buscar otras formas de integracion

---
+++ assets/listas/cliente.js
@@ -0,0 +1,6 @@
+var img_cliente = new Array(
+    'assets/img/publicidad/1920 16 RPK PLACA.jpg',
+    'assets/img/publicidad/1920 17 Monopol.jpg',
+    'assets/img/publicidad/1920 18 LASALLE.jpg'
+    
+); 
---
+++ assets/listas/pollos.js
@@ -0,0 +1,17 @@
+var img_pollos = new Array(
+    'assets/img/publicidad/1920 1 menu.jpg',
+    'assets/img/publicidad/1920 2 alitas +2.jpg',
+    'assets/img/publicidad/1920 3-COMBO-PACEÑISIMO.jpg',
+    'assets/img/publicidad/1920 4 antojito.jpg',
+    'assets/img/publicidad/1920 5 balde alitas.jpg',
+    'assets/img/publicidad/1920 6-BALDE-COPACABANA-8.jpg',
+    'assets/img/publicidad/1920 7 balde 12.jpg',
+    'assets/img/publicidad/1920 8 sandwich de pollo ahumado.jpg',
+    'assets/img/publicidad/1920 9 bicentenario.jpg',
+    'assets/img/publicidad/1920 10 combo copito.jpg',
+    'assets/img/publicidad/1920 11 copalitos.jpg',
+    'assets/img/publicidad/1920 12 agranda.jpg',
+    'assets/img/publicidad/1920 13 POSTRES-LPZ.jpg',
+    'assets/img/publicidad/1920 14 PantallaRPK_COPITO_1920x1080px_OK.jpg',
+    'assets/img/publicidad/1920 15 PantallaRPK_Complementos_1920x1080px_OK.jpg'
+); 
