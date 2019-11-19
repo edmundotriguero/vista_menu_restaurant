969d786920fac661ea647b692107e5e7776998c1
Edmundo_linux <edmundotriguero@gmail.com>
2019-11-19 09:25:32 -0400

    actualizacion con recarga de pagina a los 10 minutos

--- assets/listas/cliente.js
+++ assets/listas/cliente.js
@@ -1,6 +1,4 @@
 var img_cliente = new Array(
-    'assets/img/publicidad/1920 16 RPK PLACA.jpg',
-    'assets/img/publicidad/1920 17 Monopol.jpg',
-    'assets/img/publicidad/1920 18 LASALLE.jpg'
-    
+    'assets/img/publicidad/RPK PLACA.jpg',
+    'assets/img/publicidad/YPFB PLACA.jpg'
 ); 
